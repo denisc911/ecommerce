b87bdf9ab5d7fadaf93c1ae9cc97ed5a0f21b923
DaniCabrera91 <danielcabreravazquez91@gmail.com>
2024-06-21 20:07:55 +0200

    changed index.js file

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-<<<<<<< HEAD
 const express = require('express')
 const app = express()
+const PORT = 3000
+
 app.use(express.json())
 app.use('/products', require('./routes/products'))
 
-const PORT = 3000
 
 app.listen(PORT, () => console.log(`Server started at port ${PORT}`))
-=======
-console.log('holagit');
->>>>>>> origin
