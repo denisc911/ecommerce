e013bfd98e5ec7a875f2fded623b267072cc4329
DaniCabrera91 <danielcabreravazquez91@gmail.com>
2024-06-21 19:09:06 +0200

    Created db and the empty tables

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('hola');
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+
+const PORT = 3000
+
+app.listen(PORT, () => console.log(`Server started at port ${PORT}`))
---
+++ models/order.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Order extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Order.belongsToMany(models.Product,{
+        through:models.OrderProduct
+      })
+    }
+  }
+  Order.init({
+    order_date: DataTypes.DATE,
+    delivery_date: DataTypes.DATE,
+    UserId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Order',
+  });
+  return Order;
+};
