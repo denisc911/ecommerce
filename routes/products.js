a8398721d2546b96ce939662e3f53b5aa4109d5d
DaniCabrera91 <danielcabreravazquez91@gmail.com>
2024-06-22 11:42:29 +0200

    created CRUD of products, And started with the Users Controllers Token system. Instaled bcrypt and jsonwebtoken

--- routes/products.js
+++ routes/products.js
@@ -3,5 +3,9 @@ const router = express.Router()
 const ProductController = require('../controllers/ProductController')
 
 router.post('/', ProductController.create)
+router.get('/', ProductController.getAll)
+router.delete('/:id', ProductController.delete)
+router.put('/:id', ProductController.update)
+
 
 module.exports = router
